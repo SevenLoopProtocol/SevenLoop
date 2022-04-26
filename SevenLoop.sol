e894d538c3d863e6db5454aac89c70387eac3ec9
SevenLoopProtocol <104423828+SevenLoopProtocol@users.noreply.github.com>
2022-04-26 15:56:31 +0700

    Seven Loop Smartcontract

---
+++ SevenLoop.sol
@@ -0,0 +1,571 @@
+pragma solidity ^0.7.4;
+
+import "./ERC20Detailed.sol";
+import "./Ownable.sol";
+import "./SafeMath.sol";
+import "./SafeMathInt.sol";
+import "./IPancakeSwapPair.sol";
+import "./IPancakeSwapRouter.sol";
+import "./IPancakeSwapFactory.sol";
+
+contract SevenLoop is ERC20Detailed, Ownable {
+    using SafeMath for uint256;
+    using SafeMathInt for int256;
+
+    event LogRebase(uint256 indexed epoch, uint256 totalSupply);
+
+    string public _name = "Seven Loop";
+    string public _symbol = "SELO";
+    uint8 public _decimals = 5;
+
+    IPancakeSwapPair public pairContract;
+    mapping(address => bool) _isFeeExempt;
+
+    modifier validRecipient(address to) {
+        require(to != address(0x0));
+        _;
+    }
+
+    uint256 public constant DECIMALS = 5;
+    uint256 public constant MAX_UINT256 = ~uint256(0);
+    uint8 public constant RATE_DECIMALS = 7;
+
+    uint256 private constant INITIAL_FRAGMENTS_SUPPLY =
+        330 * 10**3 * 10**DECIMALS;
+
+    uint256 public liquidityFee = 50;
+    uint256 public treasuryFee = 20;
+    uint256 public sevenLoopInsuranceFundFee = 50;
+    uint256 public sellFee = 30;
+    uint256 public darkHoleFee = 25;
+    uint256 public lotteryFee = 5;
+    uint256 public totalFee =
+        liquidityFee
+            .add(treasuryFee)
+            .add(sevenLoopInsuranceFundFee)
+            .add(darkHoleFee)
+            .add(lotteryFee);
+    uint256 public feeDenominator = 1000;
+
+    address DEAD = 0x000000000000000000000000000000000000dEaD;
+    address ZERO = 0x0000000000000000000000000000000000000000;
+
+    address public autoLiquidityReceiver;
+    address public treasuryReceiver;
+    address public sevenLoopInsuranceFundReceiver;
+    address public lotteryReceiver;
+    address public darkHole;
+    address public pairAddress;
+    bool public swapEnabled = true;
+    IPancakeSwapRouter public router;
+    address public pair;
+    bool inSwap = false;
+    modifier swapping() {
+        inSwap = true;
+        _;
+        inSwap = false;
+    }
+
+    uint256 private constant TOTAL_GONS =
+        MAX_UINT256 - (MAX_UINT256 % INITIAL_FRAGMENTS_SUPPLY);
+
+    uint256 private constant MAX_SUPPLY = 36571000000 * 10**DECIMALS;
+
+    uint256 public _lastLotteryPayouts;
+    bool public _autoRebase;
+    bool public _autoAddLiquidity;
+    uint256 public _initRebaseStartTime;
+    uint256 public _lastRebasedTime;
+    uint256 public _lastAddLiquidityTime;
+    uint256 public _totalSupply;
+    uint256 private _gonsPerFragment;
+
+    mapping(address => uint256) private _gonBalances;
+    mapping(address => mapping(address => uint256)) private _allowedFragments;
+    mapping(address => bool) public blacklist;
+
+    constructor()
+        ERC20Detailed("Seven Loop", "SELO", uint8(DECIMALS))
+        Ownable()
+    {
+        router = IPancakeSwapRouter(0x10ED43C718714eb63d5aA57B78B54704E256024E);
+        pair = IPancakeSwapFactory(router.factory()).createPair(
+            router.WETH(),
+            address(this)
+        );
+
+        autoLiquidityReceiver = 0x0;
+        treasuryReceiver = 0x0;
+        sevenLoopInsuranceFundReceiver = 0x0;
+        lotteryReceiver = 0x0;
+        darkHole = 0x0;
+
+        _allowedFragments[address(this)][address(router)] = uint256(-1);
+        pairAddress = pair;
+        pairContract = IPancakeSwapPair(pair);
+
+        _totalSupply = INITIAL_FRAGMENTS_SUPPLY;
+        _gonBalances[treasuryReceiver] = TOTAL_GONS;
+        _gonsPerFragment = TOTAL_GONS.div(_totalSupply);
+        _initRebaseStartTime = block.timestamp;
+        _lastRebasedTime = block.timestamp;
+        _autoRebase = false;
+        _autoAddLiquidity = false;
+        _isFeeExempt[treasuryReceiver] = true;
+        _isFeeExempt[address(this)] = true;
+
+        _transferOwnership(treasuryReceiver);
+        emit Transfer(address(0x0), treasuryReceiver, _totalSupply);
+    }
+
+    function rebase() internal {
+        if (inSwap) return;
+        uint256 rebaseRate;
+        uint256 deltaTimeFromInit = block.timestamp - _initRebaseStartTime;
+        uint256 deltaTime = block.timestamp - _lastRebasedTime;
+        uint256 times = deltaTime.div(7 minutes);
+        uint256 epoch = times.mul(7);
+
+        if (deltaTimeFromInit < (365 days)) {
+            rebaseRate = 1227;
+        } else if (deltaTimeFromInit >= (7 * 365 days)) {
+            rebaseRate = 1;
+        } else if (deltaTimeFromInit >= ((15 * 365 days) / 10)) {
+            rebaseRate = 24;
+        } else if (deltaTimeFromInit >= (365 days)) {
+            rebaseRate = 146;
+        }
+
+        for (uint256 i = 0; i < times; i++) {
+            _totalSupply = _totalSupply
+                .mul((10**RATE_DECIMALS).add(rebaseRate))
+                .div(10**RATE_DECIMALS);
+        }
+
+        _gonsPerFragment = TOTAL_GONS.div(_totalSupply);
+        _lastRebasedTime = _lastRebasedTime.add(times.mul(7 minutes));
+
+        pairContract.sync();
+
+        emit LogRebase(epoch, _totalSupply);
+    }
+
+    function transfer(address to, uint256 value)
+        external
+        override
+        validRecipient(to)
+        returns (bool)
+    {
+        _transferFrom(msg.sender, to, value);
+        return true;
+    }
+
+    function transferFrom(
+        address from,
+        address to,
+        uint256 value
+    ) external override validRecipient(to) returns (bool) {
+        if (_allowedFragments[from][msg.sender] != uint256(-1)) {
+            _allowedFragments[from][msg.sender] = _allowedFragments[from][
+                msg.sender
+            ].sub(value, "Insufficient Allowance");
+        }
+        _transferFrom(from, to, value);
+        return true;
+    }
+
+    function _basicTransfer(
+        address from,
+        address to,
+        uint256 amount
+    ) internal returns (bool) {
+        uint256 gonAmount = amount.mul(_gonsPerFragment);
+        _gonBalances[from] = _gonBalances[from].sub(gonAmount);
+        _gonBalances[to] = _gonBalances[to].add(gonAmount);
+        return true;
+    }
+
+    function _transferFrom(
+        address sender,
+        address recipient,
+        uint256 amount
+    ) internal returns (bool) {
+        require(!blacklist[sender] && !blacklist[recipient], "in_blacklist");
+
+        if (inSwap) {
+            return _basicTransfer(sender, recipient, amount);
+        }
+
+        if (shouldRebase()) {
+            rebase();
+        }
+
+        if (shouldAddLiquidity()) {
+            addLiquidity();
+        }
+
+        if (shouldSwapBack()) {
+            swapBack();
+        }
+
+        uint256 gonAmount = amount.mul(_gonsPerFragment);
+        _gonBalances[sender] = _gonBalances[sender].sub(gonAmount);
+        uint256 gonAmountReceived = shouldTakeFee(sender, recipient)
+            ? takeFee(sender, recipient, gonAmount)
+            : gonAmount;
+        _gonBalances[recipient] = _gonBalances[recipient].add(
+            gonAmountReceived
+        );
+
+        emit Transfer(
+            sender,
+            recipient,
+            gonAmountReceived.div(_gonsPerFragment)
+        );
+        return true;
+    }
+
+    function takeFee(
+        address sender,
+        address recipient,
+        uint256 gonAmount
+    ) internal returns (uint256) {
+        uint256 _totalFee = totalFee;
+        uint256 _treasuryFee = treasuryFee;
+
+        if (recipient == pair) {
+            _totalFee = totalFee.add(sellFee);
+            _treasuryFee = treasuryFee.add(sellFee);
+        }
+
+        uint256 feeAmount = gonAmount.div(feeDenominator).mul(_totalFee);
+
+        _gonBalances[darkHole] = _gonBalances[darkHole].add(
+            gonAmount.div(feeDenominator).mul(darkHoleFee)
+        );
+        _gonBalances[lotteryReceiver] = _gonBalances[lotteryReceiver].add(
+            gonAmount.div(feeDenominator).mul(lotteryFee)
+        );
+        _gonBalances[address(this)] = _gonBalances[address(this)].add(
+            gonAmount.div(feeDenominator).mul(
+                _treasuryFee.add(sevenLoopInsuranceFundFee)
+            )
+        );
+        _gonBalances[autoLiquidityReceiver] = _gonBalances[
+            autoLiquidityReceiver
+        ].add(gonAmount.div(feeDenominator).mul(liquidityFee));
+
+        emit Transfer(sender, address(this), feeAmount.div(_gonsPerFragment));
+        return gonAmount.sub(feeAmount);
+    }
+
+    function addLiquidity() internal swapping {
+        uint256 autoLiquidityAmount = _gonBalances[autoLiquidityReceiver].div(
+            _gonsPerFragment
+        );
+        _gonBalances[address(this)] = _gonBalances[address(this)].add(
+            _gonBalances[autoLiquidityReceiver]
+        );
+        _gonBalances[autoLiquidityReceiver] = 0;
+        uint256 amountToLiquify = autoLiquidityAmount.div(2);
+        uint256 amountToSwap = autoLiquidityAmount.sub(amountToLiquify);
+
+        if (amountToSwap == 0) {
+            return;
+        }
+        address[] memory path = new address[](2);
+        path[0] = address(this);
+        path[1] = router.WETH();
+
+        uint256 balanceBefore = address(this).balance;
+
+        router.swapExactTokensForETHSupportingFeeOnTransferTokens(
+            amountToSwap,
+            0,
+            path,
+            address(this),
+            block.timestamp
+        );
+
+        uint256 amountETHLiquidity = address(this).balance.sub(balanceBefore);
+
+        if (amountToLiquify > 0 && amountETHLiquidity > 0) {
+            router.addLiquidityETH{value: amountETHLiquidity}(
+                address(this),
+                amountToLiquify,
+                0,
+                0,
+                autoLiquidityReceiver,
+                block.timestamp
+            );
+        }
+        _lastAddLiquidityTime = block.timestamp;
+    }
+
+    function lotteryPayouts(address[] calldata addrs) external onlyOwner {
+        require(addrs.length > 0, "Invalid addrs");
+        uint256 lotteryAmount = _gonBalances[lotteryReceiver];
+        uint256 amountPerAddr = lotteryAmount.div(addrs.length);
+        uint256 totalAmount = amountPerAddr.mul(addrs.length);
+        _lastLotteryPayouts = totalAmount.div(_gonsPerFragment);
+        _gonBalances[lotteryReceiver] = _gonBalances[lotteryReceiver].sub(
+            totalAmount
+        );
+        for (uint32 i = 0; i < addrs.length; i++) {
+            _gonBalances[addrs[i]] = _gonBalances[addrs[i]].add(amountPerAddr);
+            emit Transfer(
+                address(this),
+                addrs[i],
+                amountPerAddr.div(_gonsPerFragment)
+            );
+        }
+        emit LotteryPayouts(addrs, totalAmount.div(_gonsPerFragment));
+    }
+
+    function swapBack() internal swapping {
+        uint256 amountToSwap = _gonBalances[address(this)].div(
+            _gonsPerFragment
+        );
+
+        if (amountToSwap == 0) {
+            return;
+        }
+
+        uint256 balanceBefore = address(this).balance;
+        address[] memory path = new address[](2);
+        path[0] = address(this);
+        path[1] = router.WETH();
+
+        router.swapExactTokensForETHSupportingFeeOnTransferTokens(
+            amountToSwap,
+            0,
+            path,
+            address(this),
+            block.timestamp
+        );
+
+        uint256 amountETHToTreasuryAndSIF = address(this).balance.sub(
+            balanceBefore
+        );
+
+        (bool success, ) = payable(treasuryReceiver).call{
+            value: amountETHToTreasuryAndSIF.mul(treasuryFee).div(
+                treasuryFee.add(sevenLoopInsuranceFundFee)
+            ),
+            gas: 30000
+        }("");
+        (success, ) = payable(sevenLoopInsuranceFundReceiver).call{
+            value: amountETHToTreasuryAndSIF.mul(sevenLoopInsuranceFundFee).div(
+                treasuryFee.add(sevenLoopInsuranceFundFee)
+            ),
+            gas: 30000
+        }("");
+    }
+
+    function withdrawAllToTreasury() external swapping onlyOwner {
+        uint256 amountToSwap = _gonBalances[address(this)].div(
+            _gonsPerFragment
+        );
+        require(
+            amountToSwap > 0,
+            "There is no SELO token deposited in token contract"
+        );
+        address[] memory path = new address[](2);
+        path[0] = address(this);
+        path[1] = router.WETH();
+        router.swapExactTokensForETHSupportingFeeOnTransferTokens(
+            amountToSwap,
+            0,
+            path,
+            treasuryReceiver,
+            block.timestamp
+        );
+    }
+
+    function shouldTakeFee(address from, address to)
+        internal
+        view
+        returns (bool)
+    {
+        return (pair == from || pair == to) && !_isFeeExempt[from];
+    }
+
+    function shouldRebase() internal view returns (bool) {
+        return
+            _autoRebase &&
+            (_totalSupply < MAX_SUPPLY) &&
+            msg.sender != pair &&
+            !inSwap &&
+            block.timestamp >= (_lastRebasedTime + 7 minutes);
+    }
+
+    function shouldAddLiquidity() internal view returns (bool) {
+        return
+            _autoAddLiquidity &&
+            !inSwap &&
+            msg.sender != pair &&
+            block.timestamp >= (_lastAddLiquidityTime + 2 days);
+    }
+
+    function shouldSwapBack() internal view returns (bool) {
+        return !inSwap && msg.sender != pair;
+    }
+
+    function setAutoRebase(bool _flag) external onlyOwner {
+        if (_flag) {
+            _autoRebase = _flag;
+            _lastRebasedTime = block.timestamp;
+        } else {
+            _autoRebase = _flag;
+        }
+    }
+
+    function setAutoAddLiquidity(bool _flag) external onlyOwner {
+        if (_flag) {
+            _autoAddLiquidity = _flag;
+            _lastAddLiquidityTime = block.timestamp;
+        } else {
+            _autoAddLiquidity = _flag;
+        }
+    }
+
+    function allowance(address owner_, address spender)
+        external
+        view
+        override
+        returns (uint256)
+    {
+        return _allowedFragments[owner_][spender];
+    }
+
+    function decreaseAllowance(address spender, uint256 subtractedValue)
+        external
+        returns (bool)
+    {
+        uint256 oldValue = _allowedFragments[msg.sender][spender];
+        if (subtractedValue >= oldValue) {
+            _allowedFragments[msg.sender][spender] = 0;
+        } else {
+            _allowedFragments[msg.sender][spender] = oldValue.sub(
+                subtractedValue
+            );
+        }
+        emit Approval(
+            msg.sender,
+            spender,
+            _allowedFragments[msg.sender][spender]
+        );
+        return true;
+    }
+
+    function increaseAllowance(address spender, uint256 addedValue)
+        external
+        returns (bool)
+    {
+        _allowedFragments[msg.sender][spender] = _allowedFragments[msg.sender][
+            spender
+        ].add(addedValue);
+        emit Approval(
+            msg.sender,
+            spender,
+            _allowedFragments[msg.sender][spender]
+        );
+        return true;
+    }
+
+    function approve(address spender, uint256 value)
+        external
+        override
+        returns (bool)
+    {
+        _allowedFragments[msg.sender][spender] = value;
+        emit Approval(msg.sender, spender, value);
+        return true;
+    }
+
+    function checkFeeExempt(address _addr) external view returns (bool) {
+        return _isFeeExempt[_addr];
+    }
+
+    function getCirculatingSupply() public view returns (uint256) {
+        return
+            (TOTAL_GONS.sub(_gonBalances[DEAD]).sub(_gonBalances[ZERO])).div(
+                _gonsPerFragment
+            );
+    }
+
+    function isNotInSwap() external view returns (bool) {
+        return !inSwap;
+    }
+
+    function manualSync() external {
+        IPancakeSwapPair(pair).sync();
+    }
+
+    function setFeeReceivers(
+        address _autoLiquidityReceiver,
+        address _treasuryReceiver,
+        address _sevenLoopInsuranceFundReceiver,
+        address _darkHole,
+        address _lotteryReceiver
+    ) external onlyOwner {
+        autoLiquidityReceiver = _autoLiquidityReceiver;
+        treasuryReceiver = _treasuryReceiver;
+        sevenLoopInsuranceFundReceiver = _sevenLoopInsuranceFundReceiver;
+        darkHole = _darkHole;
+        lotteryReceiver = _lotteryReceiver;
+    }
+
+    function getLiquidityBacking(uint256 accuracy)
+        public
+        view
+        returns (uint256)
+    {
+        uint256 liquidityBalance = _gonBalances[pair].div(_gonsPerFragment);
+        return
+            accuracy.mul(liquidityBalance.mul(2)).div(getCirculatingSupply());
+    }
+
+    function setWhitelist(address _addr) external onlyOwner {
+        _isFeeExempt[_addr] = true;
+    }
+
+    function setBotBlacklist(address _botAddress, bool _flag)
+        external
+        onlyOwner
+    {
+        require(
+            isContract(_botAddress),
+            "only contract address, not allowed exteranlly owned account"
+        );
+        blacklist[_botAddress] = _flag;
+    }
+
+    function setPairAddress(address _pairAddress) public onlyOwner {
+        pairAddress = _pairAddress;
+    }
+
+    function setLP(address _address) external onlyOwner {
+        pairContract = IPancakeSwapPair(_address);
+    }
+
+    function totalSupply() external view override returns (uint256) {
+        return _totalSupply;
+    }
+
+    function balanceOf(address who) external view override returns (uint256) {
+        return _gonBalances[who].div(_gonsPerFragment);
+    }
+
+    function isContract(address addr) internal view returns (bool) {
+        uint256 size;
+        assembly {
+            size := extcodesize(addr)
+        }
+        return size > 0;
+    }
+
+    receive() external payable {}
+
+    event LotteryPayouts(address[] indexed addrs, uint256 amount);
+}
